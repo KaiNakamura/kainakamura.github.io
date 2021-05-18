9d01205b4d6400487f6cdeada09e69ee2d990636
KaiNakamura <kai@nakamura-online.com>
2021-05-18 10:37:31 -0700

    Load js from single file

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-// const scripts = [
-// 	"navbar.js",
-// 	"sidebar.js",
-// 	"icon.js"
-// ];
+const scripts = [
+	"navbar.js",
+	"sidebar.js",
+	"icon.js"
+];
 
-// function loadScripts() {
-// 	for (let script of scripts) {
-// 		// Create scrip element
-// 		let scriptElement = document.createElement("script");
-// 		scriptElement.type = "text/javascript";
-// 		scriptElement.src = "/js/" + script;
+function loadScripts() {
+	for (let script of scripts) {
+		// Create scrip element
+		let scriptElement = document.createElement("script");
+		scriptElement.type = "text/javascript";
+		scriptElement.src = "/js/" + script;
 
-// 		// Append script element to head
-// 		document.head.appendChild(scriptElement);
-// 	}
-// }
+		// Append script element to head
+		document.head.appendChild(scriptElement);
+	}
+}
 
-// loadScripts();
+loadScripts();
 
 window.onload = function() {
 	loadNavbar();
