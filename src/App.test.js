6c7bc140f12552806c5bd01a432c6cd7bf044324
KaiNakamura <kaihnakamura@gmail.com>
2021-05-20 22:33:54 -0700

    Remove profile picture test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders profile picture', () => {
-  render(<App />);
-  const profilePicture = screen.getByAltText("Kai Nakamura");
-  expect(profilePicture).toBeInTheDocument();
+test('empty test', () => {
 });
