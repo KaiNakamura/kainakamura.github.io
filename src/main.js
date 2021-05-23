bb7702804b82b7f4eccc7da0ff7750f989998a92
KaiNakamura <kaihnakamura@gmail.com>
2021-05-22 23:30:33 -0700

    Add relative path to threejs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import '../style.css'
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three';
 import Object3D from './Object3D';
 import Star from './Star';
 import ProfilePicture from './ProfilePicture';
