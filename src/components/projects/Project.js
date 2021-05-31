d964e6982c44e5bd4d16e0c4e4f76cef425fa83d
KaiNakamura <kaihnakamura@gmail.com>
2021-05-31 13:38:08 -0700

    Resized components for mobile view

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -5,12 +5,14 @@ export default class Project extends React.Component {
 	render() {
 		return (
 			<div className='Project'>
-				<div className='ProjectText'>
+				<div className='ProjectHeading'>
 					<h2 className={this.props.language}>{this.props.language}</h2>
 					<h1>{this.props.title}</h1>
-					{this.props.children}
 				</div>
 				{this.props.img}
+				<div className='ProjectDescription'>
+					{this.props.children}
+				</div>
 			</div>
 		);
 	}
