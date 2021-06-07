b20993cbfe09ee8a4c4730f40baf6c21f13bc061
KaiNakamura <kaihnakamura@gmail.com>
2021-06-06 23:17:26 -0700

    Rewording about section

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -20,7 +20,7 @@ const HomePage = () => (
 			<div></div>
 		</div>
 		<section className='Content'>
-			<p>I like programming robots, websites, games, and whatever else comes to mind. Right now I'm pursuing my Bachelor's in Computer Science at <a href='https://wpi.edu' target='_blank' rel='noreferrer noopener'>WPI</a>.</p>
+			<p>I like programming robots, websites, games, and whatever else comes to mind. Right now I'm pursuing a Bachelor's in Computer Science at <a href='https://wpi.edu' target='_blank' rel='noreferrer noopener'>WPI</a>.</p>
 			<p>Check out some of the cool stuff I've been working on below. If you've got any questions or are interested in working with me feel free to <Link to='contact'>shoot me an email</Link>.</p>
 
 		</section>
