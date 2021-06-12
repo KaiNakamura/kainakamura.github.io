50e4083f9ea49a8662b78a3f9bd8b41191f12087
KaiNakamura <kaihnakamura@gmail.com>
2021-06-11 23:02:17 -0700

    Add hero background canvas, remove unused sidebar files

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -6,23 +6,25 @@ import './HomePage.css';
 const HomePage = () => (
 	<div className='HomePage'>
 		<section className='Hero'>
-			<div className='Title'>
-				<h1>Hi, I'm Kai</h1>
-				<h2>Software Dev / Student</h2>
+			<canvas className='HeroBackground'></canvas>
+			<div className='HeroContent'>
+				<div className='Title'>
+					<h1>Hi, I'm Kai</h1>
+					<h2>Software Dev / Student</h2>
+				</div>
+				<img className='ProfilePicture' src={pfp} alt='Kai Nakamura' />
 			</div>
-			<img className='ProfilePicture' src={pfp} alt='Kai Nakamura' />
+			<div className='Arrow'>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
 		</section>
-		<div className='Arrow'>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
 		<section className='Content'>
 			<p>I like programming robots, websites, games, and whatever else comes to mind. Right now I'm pursuing a Bachelor's in Computer Science at <a href='https://wpi.edu' target='_blank' rel='noreferrer noopener'>WPI</a>.</p>
 			<p>Check out some of the cool stuff I've been working on below. If you've got any questions or are interested in working with me feel free to <Link to='contact'>shoot me an email</Link>.</p>
-
 		</section>
 		<Projects />
 	</div>
