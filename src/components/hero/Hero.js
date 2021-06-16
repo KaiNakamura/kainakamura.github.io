ed57c67f5ccbdfce7bfce2a3c2557b19d701d93c
KaiNakamura <kaihnakamura@gmail.com>
2021-06-15 18:12:33 -0700

    Add unselectable class, make arrow unselectable

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,14 +8,14 @@ export default class Hero extends React.Component {
 		return (
 			<section className='Hero'>
 				<Boids />
-				<div className='HeroContent'>
+				<div className='HeroContent unselectable'>
 					<div className='Title'>
 						<h1>Hi, I'm Kai</h1>
 						<h2>Software Dev / Student</h2>
 					</div>
 					<img className='ProfilePicture' src={pfp} alt='Kai Nakamura' />
 				</div>
-				<div className='Arrow'>
+				<div className='Arrow unselectable'>
 					<div></div>
 					<div></div>
 					<div></div>
