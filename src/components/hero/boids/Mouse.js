e996142404bb47ae1c02e272b33a1a2dc335e0dd
KaiNakamura <kaihnakamura@gmail.com>
2021-06-15 18:40:33 -0700

    Animate boid cursor on click

--- src/components/hero/boids/Mouse.js
+++ src/components/hero/boids/Mouse.js
@@ -1,17 +1,27 @@
+const ANIMATION_FRAMES = 12;
+
 export default class Mouse {
 	constructor(sketch) {
 		this.sketch = sketch;
 		this.radius = 100;
+		this.animationFrame = 0;
 	}
 
 	draw() {
+		if (this.sketch.mouseIsPressed && this.animationFrame < ANIMATION_FRAMES) {
+			this.animationFrame++;
+		}
+		else if (!this.sketch.mouseIsPressed && this.animationFrame > 0) {
+			this.animationFrame--;
+		}
+
 		this.sketch.stroke(100);
 		this.sketch.strokeWeight(2);
 		this.sketch.noFill();
 		this.sketch.ellipse(
 			this.sketch.mouseX,
 			this.sketch.mouseY,
-			this.radius * 2
+			this.radius * 2 - this.easeInOutQuad(this.animationFrame)
 		);
 
 		this.sketch.noStroke();
@@ -19,7 +29,12 @@ export default class Mouse {
 		this.sketch.ellipse(
 			this.sketch.mouseX,
 			this.sketch.mouseY,
-			this.radius / 4.0
+			this.radius / 4.0 + 2 * this.easeInOutQuad(this.animationFrame)
 		);
+	}
+
+	easeInOutQuad(animationFrame) {
+		let t = animationFrame / ANIMATION_FRAMES;
+		return (t < 0.5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2) * ANIMATION_FRAMES;
 	}
 }
