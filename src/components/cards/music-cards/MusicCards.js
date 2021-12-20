124955464d322bdc5148305da3c86cb39b79dae0
KaiNakamura <kaihnakamura@gmail.com>
2021-12-19 23:41:41 -0800

    Add music cards

--- src/components/cards/music-cards/MusicCards.js
+++ src/components/cards/music-cards/MusicCards.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import Card from 'components/cards/Card';
 import CardAudio from 'components/cards/CardAudio';
-import final_project from 'audio/Electronic_Music_Composition_Final_Project.mp3';
+import gravity_cycles from 'audio/Gravity_Cycles.mp3';
 import there_will_come_soft_rains from 'audio/There_Will_Come_Soft_Rains.mp3';
+import dark_blue_255 from 'audio/Dark_Blue_255.mp3';
+import saccharine_squares from 'audio/Saccharine_Squares.mp3';
 import 'components/cards/Cards.css';
 
 export default class ProjectCards extends React.Component {
 	render() {
 		return (
 			<div className='Cards'>
-				<Card title='Electronic Music Composition Final Project'>
+				<Card title='Gravity Cycles'>
 					<p>I made this piece for my final project in my Electronic Music Composition class taught by professor <a href='https://www.vjmanzo.com/' target='_blank' rel='noreferrer noopener'>V.J. Manzo</a>.</p>
 					<p>I wanted to make something that would showcase what I've learned over the duration of this course. So I took several unfinished projects I made before taking this class and remixed them all together in the style of a chiptune song.</p>
-					<CardAudio audio={final_project} />
+					<CardAudio audio={gravity_cycles} />
 				</Card>
 				<Card title='There Will Come Soft Rains'>
 					<p>I was inspired to make this piece by a chapter from my favorite book, <a href='https://en.wikipedia.org/wiki/The_Martian_Chronicles' target='_blank' rel='noreferrer noopener'>The Martian Chronicles</a> by Ray Bradbury.</p>
 					<p>I used voice lines from an audiobook to tell the story and experimented with different instrumentation to fit the mood.</p>
 					<CardAudio audio={there_will_come_soft_rains} />
+				</Card>
+				<Card title='Dark Blue 255'>
+					<p>An ambient piece I made by using a very spacy pad with lots of reverb. I also added some frequency sweeps so the sound of the pad is always changing.</p>
+					<p>All the while a random number generator plays notes on a quiet bell.</p>
+					<CardAudio audio={dark_blue_255} />
+				</Card>
+				<Card title='Saccharine Squares'>
+					<p>A fun 8-bit chiptune melody I wrote to be used for a 2D platformer.</p>
+					<CardAudio audio={saccharine_squares} />
 				</Card>
 			</div>
 		);
