07edc7107c29d3381c9a3f2be5b4e0c8438b53b4
KaiNakamura <kaihnakamura@gmail.com>
2022-03-26 14:30:22 -0400

    Update music

--- src/components/cards/music-cards/MusicCards.js
+++ src/components/cards/music-cards/MusicCards.js
@@ -4,10 +4,10 @@ import Cards from 'components/cards/Cards';
 import CardLink from 'components/cards/CardLink';
 import CardAudio from 'components/cards/CardAudio';
 import gravity_cycles from 'audio/Gravity_Cycles.mp3';
-import there_will_come_soft_rains from 'audio/There_Will_Come_Soft_Rains.mp3';
-import deep_blue_255 from 'audio/Deep_Blue_255.mp3';
-import saccharine_squares from 'audio/Saccharine_Squares.mp3';
+import timbercreek from 'audio/Timbercreek.mp3';
 import reverse_entropy from 'audio/Reverse_Entropy.mp3';
+import deep_blue_255 from 'audio/Deep_Blue_255.mp3';
+import there_will_come_soft_rains from 'audio/There_Will_Come_Soft_Rains.mp3';
 import 'components/cards/Cards.css';
 
 export default class ProjectCards extends React.Component {
@@ -15,27 +15,24 @@ export default class ProjectCards extends React.Component {
 		return (
 			<Cards slideshow={this.props.slideshow}>
 				<Card title='Gravity Cycles'>
-					<p>I made this piece for my final project in my Electronic Music Composition class taught by professor <CardLink href='https://www.vjmanzo.com/'>V.J. Manzo</CardLink>.</p>
-					<p>I wanted to make something that would showcase what I've learned over the duration of this course. So I took several unfinished projects I made before taking this class and remixed them all together in the style of a chiptune song.</p>
+					<p>My stab at creating an EDM beat with some different synths I made.</p>
 					<CardAudio audio={gravity_cycles} />
 				</Card>
-				<Card title='There Will Come Soft Rains'>
-					<p>I was inspired to make this piece by a chapter from my favorite book, <CardLink href='https://en.wikipedia.org/wiki/The_Martian_Chronicles'>The Martian Chronicles</CardLink> by Ray Bradbury.</p>
-					<p>I used voice lines from an audiobook to tell the story and experimented with different instrumentation to fit the mood.</p>
-					<CardAudio audio={there_will_come_soft_rains} />
-				</Card>
-				<Card title='Deep Blue 255'>
-					<p>An ambient piece I made by using a very spacy pad with lots of reverb. I also added some frequency sweeps so the sound of the pad is always changing.</p>
-					<p>All the while a random number generator plays notes on a quiet bell.</p>
-					<CardAudio audio={deep_blue_255} />
-				</Card>
-				<Card title='Saccharine Squares'>
-					<p>A fun 8-bit chiptune melody I wrote to be used for a 2D platformer.</p>
-					<CardAudio audio={saccharine_squares} />
+				<Card title='Timbercreek'>
+					<p>My final composition for Topics in MIDI taught by Professor <CardLink href='https://www.davidibbett.com/'>David Ibbett</CardLink>.</p>
+					<CardAudio audio={timbercreek} />
 				</Card>
 				<Card title='Reverse Entropy'>
-					<p>My stab at creating an EDM beat with some different synths I made.</p>
+					<p>I made this 8-bit chiptune piece for my final project in my Electronic Music Composition class taught by Professor <CardLink href='https://www.vjmanzo.com/'>V.J. Manzo</CardLink>.</p>
 					<CardAudio audio={reverse_entropy} />
+				</Card>
+				<Card title='Deep Blue 255'>
+					<p>An ambient piece I made by using a spacy pad with lots of reverb. In the background, a random number generator plays a quiet kalimba. </p>
+					<CardAudio audio={deep_blue_255} />
+				</Card>
+				<Card title='There Will Come Soft Rains'>
+					<p>An experimental piece inspired from a chapter of <CardLink href='https://en.wikipedia.org/wiki/The_Martian_Chronicles'>The Martian Chronicles</CardLink> by Ray Bradbury.</p>
+					<CardAudio audio={there_will_come_soft_rains} />
 				</Card>
 			</Cards>
 		);
